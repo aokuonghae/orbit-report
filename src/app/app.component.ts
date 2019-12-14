ee62410967390600a3ce7bee8d7b8610c29878c2
Anastasia Okuonghae <aokuonghae@gmail.com>
2019-12-13 20:40:29 -0600

    highlight space debris

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,18 +16,13 @@ export class AppComponent {
     window.fetch(satellitesUrl).then(function(response){
       response.json().then(function(data){
         
-        let fetchedSatellites = data.satellites;
-        // TODO: loop over satellites
+        let fetchedSatellites = data.satellites;      
         for (let i=0; i<fetchedSatellites.length; i++){
           let satellite=new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           this.sourceList.push(satellite);
+          console.log (satellite.shouldShowWarning())
         }
-
-
-         // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-         // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
-
       }.bind(this));      
     }.bind(this));
- }
+  }
 }
