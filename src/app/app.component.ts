bed913570d5b1ef5041d2205134abdebcaa94373
Anastasia Okuonghae <aokuonghae@gmail.com>
2019-12-14 23:21:09 -0600

    update search(type) feature bonus mission finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
 
 
   constructor() {
-    this.displayList = [];
+    this.displayList = this.sourceList;
     this.sourceList = [];
     let satellitesUrl= 'https://handlers.education.launchcode.org/static/satellites.json';
 
@@ -23,24 +23,22 @@ export class AppComponent {
         let fetchedSatellites = data.satellites;      
         for (let i=0; i<fetchedSatellites.length; i++){
           let satellite=new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-          this.sourceList.push(satellite);          
+          this.sourceList.push(satellite);   
+          this.displayList = this.sourceList.slice(0);       
         }
       }.bind(this));      
     }.bind(this));     
   }
-//   search(searchTerm: string): void {
-//     let matchingSatellites: Satellite[] = [];
-//     searchTerm = searchTerm.toLowerCase();
-//     for(let i=0; i < this.sourceList.length; i++) {
-//        let name = this.sourceList[i].name.toLowerCase();
-//        if (name.indexOf(searchTerm) >= 0) {
-//           matchingSatellites.push(this.sourceList[i]);
-//        }
-//     }
-//     // assign this.displayList to be the the array of matching satellites
-//     // this will cause Angular to re-make the table, but now only containing matches
-//     this.displayList = matchingSatellites;
-
-//  }
-// Even when the search search Function is commented out, there is still a range error.
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       let type = this.sourceList[i].type.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0 || type.indexOf(searchTerm)>=0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    this.displayList = matchingSatellites;
+  }
 }
