853cefa10848f638ad573f08d18bc87536f8451e
Anastasia Okuonghae <aokuonghae@gmail.com>
2019-12-16 20:52:15 -0600

    completed assignment

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -15,7 +15,18 @@ export class OrbitCountsComponent implements OnInit {
   ngOnInit() {
   }
 
-  count(){    
+  count(type){
+    let counter=0
+    for (let i=0; i<this.satellites.length; i++){
+      if (this.satellites[i].type.toLowerCase() === type.toLowerCase()){
+        counter += 1
+      }
+    }
+    return counter;
+  }
+
+  total(){
+    return this.satellites.length;
   }
 
 }
